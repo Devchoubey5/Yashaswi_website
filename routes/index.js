59df7a73e036ef830e005d5c7c28443210c2c4d0
DEVENDRA CHOUBEY <devchoubey5@gmail.com>
2022-10-10 14:55:55 +0530

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,29 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/testingApp', function(req, res, next) {
+router.get('/testingApp', function(req, res) {
   res.render('testing_js');
 });
-router.get('/Registration', function(req, res, next) {
+router.get('/Registration', function(req, res) {
   res.render('Registration');
 });
-router.get('/blog-single', function(req, res, next) {
+router.get('/blog-single', function(req, res) {
   res.render('blog-single');
 });
-router.get('/lk', function(req, res, next) {
+router.get('/blog', function(req, res) {
   res.render('blog');
 });
+router.get('/blog-single', function(req, res) {
+  res.render('blog-single');
+});
+router.get('/browsejobs', function(req, res) {
+  res.render('browsejobs');
+});
+router.get('/candidates', function(req, res) {
+  res.render('candidates');
+});
+router.get('/job-post', function(req, res) {
+  res.render('job-post');
+});
+
 module.exports = router;
